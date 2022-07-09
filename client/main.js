23db0451ea60d23e71943c7d04839d66513aed8c
Shipalan <Shipalan678@gmail.com>
2022-07-08 19:53:07 -0600

    stopping point 7/8

--- client/main.js
+++ client/main.js
@@ -2,29 +2,28 @@ let milesInputForm = document.getElementById("milesInputForm");
 let milesInput = document.getElementById("milesInput");
 const date = new Date();
 
-// function newLog(body) {
-//   console.log(body);
-// }
+function newLog(body) {
+  axios.post("/api/log", body).then((res) => {
+    console.log("this is body", body);
+  });
+  console.log(body);
+}
 
 function inputFormHandler(event) {
   event.preventDefault();
-  
+
   const date = new Date();
   let currentDate = `${
     date.getMonth() + 1
   }-${date.getDate()}-${date.getFullYear()}`;
   // console.log(currentDate);
-  
+
   let body = {
     date: currentDate,
     miles: `${milesInput.value} miles`,
   };
-  
-  axios.post("/api/log", body).then((res) => {
-    res.send(body)
-  });
-  console.log('this is body' , body);
-  // newLog(body);
+
+  newLog(body);
   milesInput.value = "";
 }
 
